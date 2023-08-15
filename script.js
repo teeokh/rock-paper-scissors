791c7283b41e9d85255ab6041fe0b7f805c4d25a
Tee <139244119+teeokh@users.noreply.github.com>
2023-08-15 17:54:10 +0100

    Create function playRound()
    Create function for playing one round of RPS. Passes playerSelection (from user input using prompt()) and computerSelection (from getComputerChoice()) as parameters for playRound() function.
    
    If user input does not include rock, paper or scissors, "wrong input" is returned.

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 
 
-function getComputerChoice(){
-    const options = ["Rock", "Paper", "Scissors"]; // create array of these options
+   function getComputerChoice(){
+    const options = ["rock", "paper", "scissors"]; // create array of these options
 
     let index = Math.floor(Math.random() * options.length); // get random index based off the numbers available in this array
 
     return options[index]; // print the corresponding index of the words available in the array 
-    
 }
 
-console.log(getComputerChoice());
+    let playerSelection = prompt("Enter rock, paper or scissors").toLowerCase();
+    let computerSelection = getComputerChoice();
+
+ function playRound(playerSelection, computerSelection){
+
+    let validChoices = ["rock", "paper", "scissors"];
+
+    if (!validChoices.includes(playerSelection)){
+        return "Wrong input!"
+    }
+
+    if (playerSelection == "rock"){
+        if (computerSelection == "paper"){
+            return "You Lose! Paper beats Rock"
+        } else if (computerSelection == "scissors"){
+            return "You Win! Rock beats Scissors"
+        } else {
+            return "Draw!"
+        }
+    }
+
+    if (playerSelection == "paper"){
+        if (computerSelection == "scissors"){
+            return "You Lose! Scissors beats Paper"
+        } else if (computerSelection == "rock"){
+            return "You Win! Paper beats Rock"
+        } else {
+            return "Draw!"
+        }
+    }
+
+    if (playerSelection == "scissors"){
+        if (computerSelection == "paper"){
+            return "You Win! Scissors beats Paper"
+        } else if (computerSelection == "rock"){
+            return "You Lose! Rock beats Scissors"
+        } else {
+            return "Draw!"
+        }
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+
